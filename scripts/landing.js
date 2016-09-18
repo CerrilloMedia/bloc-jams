825b6bca6f0107b55d3779b448d0f1dd59aadc19
Armando Cerrillo <armando@cerrillomedia.org>
2016-09-18 00:16:33 -0700

    adjust landing.js with new style

--- scripts/landing.js
+++ scripts/landing.js
@@ -4,6 +4,8 @@ var animatePoints = function() {
     
     var revealPoint = function(arg) {
             points[arg].style.opacity = 1;
+            points[i].style.color = "rgb(233,50,117)";
+            points[i].getElementsByTagName('span')[0].style.color = "rgb(255,255,255)";
             points[arg].style.transform = "scaleX(1) translateY(0)";
             points[arg].style.msTransform = "scaleX(1) translateY(0)";
             points[arg].style.WekitTransform = "scaleX(1) translateY(0)";
@@ -12,7 +14,5 @@ var animatePoints = function() {
     for ( var i = 0; i < points.length ; i++) {
         revealPoint(i);
     }
-        
-    //revealThirdPoint();
 
 };
