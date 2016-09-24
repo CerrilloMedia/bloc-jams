4abf9e6b4f17104b03b9cc8143b297c6707d6425
Armando Cerrillo <armando@cerrillomedia.org>
2016-09-23 18:42:16 -0700

    fix current playing track bug

--- scripts/album.js
+++ scripts/album.js
@@ -172,7 +172,7 @@ window.onload = function() {
             if (songItemNumber !== currentPlayingSong) {
                 songItem.innerHTML = songItemNumber;
             } else { // without this code, re-hovering (not clicking) over the pause icon sets it back to play prematurely
-                songItem.innerHTML = pauseButtonTemplate;
+                songItem.innerHTML = playButtonTemplate;
             }
             
         });
